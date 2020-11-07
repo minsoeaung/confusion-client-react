9f4a18134d6029139ddde90c4ac5827f0cfd5cb2
minsoeaung <minsoeaung2001@gmail.com>
2020-11-07 16:04:40 +0630

    React Animations

--- src/components/LoadingComponent.js
+++ src/components/LoadingComponent.js
@@ -8,3 +8,18 @@ export const Loading = () => {
         </div>
     );
 }
+
+// import LoadingMask from "react-loadingmask";
+// import "react-loadingmask/dist/react-loadingmask.css";
+
+
+
+// export const Loading = () => {
+//     return (
+//         <div>
+//             <LoadingMask loading={true} loadingText={"loading..."}>
+//               <div style={{ width: 300, height: 300 }}></div>
+//             </LoadingMask>
+//         </div>
+//     );
+// }
